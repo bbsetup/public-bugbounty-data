ae9593d2fe566196bfb2f76b38fc6f26a3c7ec69
bbsetup <77829284+bbsetup@users.noreply.github.com>
2021-01-23 02:58:27 +0530

    Recon DATA

---
+++ booth/accounts.booth.pm
@@ -0,0 +1,63 @@
+```<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>BOOTH | The page you were looking for doesn't exist (404)</title>
+  <style>
+  body {
+    margin: 0;
+    color: #2E2F30;
+    text-align: center;
+    font-family: arial, sans-serif;
+  }
+
+  .not_found_girl {
+    display: block;
+    width: 160px;
+    height: 206px;
+    margin: 0 auto 10px auto;
+    background: url('/static-images/error/not_found_girl.png') no-repeat;
+    text-indent: -9999pt;
+  }
+
+  @media only screen and (-webkit-min-device-pixel-ratio: 2), only screen and (min-resolution: 192dpi) {
+    .not_found_girl {
+      background: url('/static-images/error/not_found_girl@2x.png') no-repeat;
+      background-size: contain;
+    }
+  }
+
+  .not_found_logo {
+    display: block;
+    width: 400px;
+    height: 87px;
+    margin: 0 auto 24px auto;
+    background: url('/static-images/error/not_found_logo.png') no-repeat;
+    text-indent: -9999pt;
+  }
+
+  @media only screen and (-webkit-min-device-pixel-ratio: 2), only screen and (min-resolution: 192dpi) {
+    .not_found_logo {
+      background: url('/static-images/error/not_found_logo@2x.png') no-repeat;
+      background-size: contain;
+    }
+  }
+
+  .dialog {
+      height: 300px;
+      padding: 10px 0 248px 0;
+      background: #FFF;
+  }
+  </style>
+</head>
+
+<body>
+  <div class="dialog">
+    <span class="not_found_girl" title=""></span>
+    <a href="/" class="not_found_logo" title="Jump to BOOTH TOP">BOOTH</a>
+    <h1>The page you were looking for doesn't exist.</h1>
+    <p>You may have mistyped the address or the page may have moved.</p>
+  </div>
+</body>
+</html>
+```
